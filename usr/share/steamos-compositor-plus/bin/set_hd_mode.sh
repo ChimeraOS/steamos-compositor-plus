491eb86444afc3fc06c9f10b09c72cac5c1b53a3
Alesh Slovak <aleshslovak@gmail.com>
2021-08-14 00:26:54 -0400

    add 16:10 resolutions (for handhelds)

--- usr/share/steamos-compositor-plus/bin/set_hd_mode.sh
+++ usr/share/steamos-compositor-plus/bin/set_hd_mode.sh
@@ -27,7 +27,7 @@ function first_by_prefix_order() {
     done
 }
 
-GOODMODES=("3840x2160" "2560x1440" "1920x1080" "1280x720")
+GOODMODES=("3840x2160" "2560x1600" "2560x1440" "1920x1200" "1920x1080" "1280x800" "1280x720")
 GOODRATES=("60.0" "59.9") # CEA modes guarantee or one the other, but not both?
 ROTATION=
 
@@ -38,7 +38,7 @@ CONFIG_FILE="$CONFIG_PATH/steamos-compositor-plus"
 if [ -f "$CONFIG_FILE" ]; then
     source "$CONFIG_FILE"
 else
-    echo '#GOODMODES=("3840x2160" "2560x1440" "1920x1080" "1280x720")' > "$CONFIG_FILE"
+    echo '#GOODMODES=("3840x2160" "2560x1600" "2560x1440" "1920x1200" "1920x1080" "1280x800" "1280x720")' > "$CONFIG_FILE"
     echo '#GOODRATES=("60.0" "59.9")' >> "$CONFIG_FILE"
     echo '#ROTATION=' >> "$CONFIG_FILE"
 fi
