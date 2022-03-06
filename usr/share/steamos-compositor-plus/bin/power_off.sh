e56e1073950534babba5ec3c05e204ca4350f348
Derek J. Clark <derekjohn.clark@gmail.com>
2022-03-05 17:06:19 -0800

    Suspend/Poweroff support - Added power_off.sh script to check for single or double press of the power button. Single press goes into suspend. Double press goes into shutdown. - Modified sxhkdrc to use the power_off.sh script for FX86PowerOff hook.

---
+++ usr/share/steamos-compositor-plus/bin/power_off.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+NAME=$(basename $0)
+PID=$$
+PIDFILE=/tmp/$NAME.pid
+MAXSEC=2
+
+trap "rm -f $PIDFILE" EXIT
+
+if pidof -o %PPID -x $NAME >/dev/null; then
+        echo $PID > $PIDFILE
+	sleep $MAXSEC
+	exit 0
+fi
+
+while [ $SECONDS -lt $MAXSEC ]; do
+  if [ -f "$PIDFILE" ]; then 
+    OTHERPID=`cat $PIDFILE`
+    kill $OTHERPID
+    systemctl shutdown
+    exit 0
+  fi
+done
+systemctl suspend
+exit 0
