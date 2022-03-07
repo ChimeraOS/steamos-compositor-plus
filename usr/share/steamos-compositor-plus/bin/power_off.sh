3d6dc96a59c86dffef43e0f0c24b9abd6c0342ee
Derek J. Clark <derkejohn.clark@gmail.com>
2022-03-07 08:52:11 -0800

    Update power_off.sh
    Fixed spacing in the file. Tabs vs spaces...

--- usr/share/steamos-compositor-plus/bin/power_off.sh
+++ usr/share/steamos-compositor-plus/bin/power_off.sh
@@ -8,18 +8,19 @@ MAXSEC=2
 trap "rm -f $PIDFILE" EXIT
 
 if pidof -o %PPID -x $NAME >/dev/null; then
-        echo $PID > $PIDFILE
-	sleep $MAXSEC
-	exit 0
+    echo $PID > $PIDFILE
+    sleep $MAXSEC
+    exit 0
 fi
 
 while [ $SECONDS -lt $MAXSEC ]; do
-  if [ -f "$PIDFILE" ]; then 
-    OTHERPID=`cat $PIDFILE`
-    kill $OTHERPID
-    systemctl poweroff
-    exit 0
-  fi
+    if [ -f "$PIDFILE" ]; then 
+        OTHERPID=`cat $PIDFILE`
+        kill $OTHERPID
+        systemctl poweroff
+        exit 0
+    fi
 done
+
 systemctl suspend
 exit 0
